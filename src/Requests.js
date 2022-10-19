9f4805ef0750faad1b2e17a5d644a630abe93968
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-10-19 00:00:54 -0400

    Add Requests.js

---
+++ src/Requests.js
@@ -0,0 +1,13 @@
+const key = process.env.REACT_APP_THEMOVIEDB_API_KEY
+
+console.log(key)
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+}
+
+export default requests
