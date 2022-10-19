7bbd4ba949325e779cdf6428c7cb7c2a4ef72fdc
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-10-19 00:02:17 -0400

    Cleanup requests

--- src/Requests.js
+++ src/Requests.js
@@ -1,7 +1,5 @@
 const key = process.env.REACT_APP_THEMOVIEDB_API_KEY
 
-console.log(key)
-
 const requests = {
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
