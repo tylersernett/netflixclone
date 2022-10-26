8f62800ae86fd50dcb90d7060c3808fe707a1587
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-10-26 16:38:35 -0400

    Fix word-wrap bug

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -75,7 +75,7 @@ const Movie = ({ setHeroMovie, item }) => {
         <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor:pointer relative p-2'>
             <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
             <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
-                <p onClick={updateHeroMovie } className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center cursor-pointer'>
+                <p onClick={updateHeroMovie } className='whitespace-normal  text-xs md:text-sm font-bold flex justify-center items-center h-full text-center cursor-pointer'>
                     {item?.title}
                 </p>
                 <p onClick={saveShow}>
