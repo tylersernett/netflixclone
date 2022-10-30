36634ae6e3893bcf5e1085879ec9049c84d991d4
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-10-30 19:57:31 -0400

    Add "re-liking" on account page

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -1,18 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import { UserAuth } from '../context/AuthContext'
 import { db } from '../firebase'
-import { doc, onSnapshot } from 'firebase/firestore'
+import { doc, getDoc, onSnapshot } from 'firebase/firestore'
 import Row from './Row'
 
-const SavedShows = ({setHeroMovie}) => {
+const SavedShows = ({ setHeroMovie }) => {
     const [userMovies, setUserMovies] = useState([]);
     const { user } = UserAuth();
 
+    //userMovies are only refreshed on first page load, so user can like/unlike from account page:
     useEffect(() => {
-        onSnapshot(doc(db, 'users', `${user?.email}`), (doc)=>{
-            setUserMovies(doc.data()?.savedShows)
-        })
-    }, [user?.email])  
+        const getUserMovies = async () => {
+            const userData = doc(db, 'users', `${user?.email}`)
+            const userSnap = await getDoc(userData); //all data -- use .savedShows later to just grab the array
+            try {
+                setUserMovies( userSnap.data().savedShows );
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getUserMovies();
+    
+    }, [user?.email])
+    
+    //use the following if you want instant refresh of deleted items: (use can't re-like an unliked item)
+    // useEffect(() => {
+    //     onSnapshot(doc(db, 'users', `${user?.email}`), (doc)=>{
+    //         setUserMovies(doc.data()?.savedShows)
+    //     })
+    // }, [user?.email])  
 
     return (
         <>
