fa55afade680a8c5ad04b0a30118d4c7edd10130
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-11-15 01:43:56 -0500

    Fix typo: w-fill --> w-full

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -29,7 +29,7 @@ const Row = ({ setHeroMovie, title, movieArrayData }) => {
             <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
             <div className='relative flex items-center group'>
                 <MdChevronLeft onClick={() => slideScroll(slider, -500)} className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
-                <div ref={slider} className='w-fill h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+                <div ref={slider} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
                     {movies.map((item, id) => (
                         <Movie setHeroMovie={setHeroMovie} item={item} key={id} />
                     ))}
