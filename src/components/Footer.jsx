b8575fe82457f065e72bc2bad8ed8cf497e20661
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-12-07 14:39:00 -0500

    Add pseudolinks to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,23 @@
 
 const footerPages = [
-    { text: "FAQ", link: "" },
-    { text: "Help Center", link: "" },
-    { text: "Account", link: "" },
-    { text: "Media Center", link: "" },
-    { text: "Investor Relations", link: "" },
-    { text: "Jobs", link: "" },
-    { text: "Redeem Gift Cards", link: "" },
-    { text: "Buy Gift Cards", link: "" },
-    { text: "Ways to Watch", link: "" },
-    { text: "Terms of Use", link: "" },
-    { text: "Privacy", link: "" },
-    { text: "Cookie Preferences", link: "" },
-    { text: "Corporate Information", link: "" },
-    { text: "Contact Us", link: "" },
-    { text: "Speed Test", link: "" },
-    { text: "Legal Notices", link: "" },
-    { text: "Only on Netflix", link: "" },
-    { text: "Do Not Sell or Share My Personal Information", link: "" },
+    { text: "FAQ", link: "/" },
+    { text: "Help Center", link: "/" },
+    { text: "Account", link: "/" },
+    { text: "Media Center", link: "/" },
+    { text: "Investor Relations", link: "/" },
+    { text: "Jobs", link: "/" },
+    { text: "Redeem Gift Cards", link: "/" },
+    { text: "Buy Gift Cards", link: "/" },
+    { text: "Ways to Watch", link: "/" },
+    { text: "Terms of Use", link: "/" },
+    { text: "Privacy", link: "/" },
+    { text: "Cookie Preferences", link: "/" },
+    { text: "Corporate Information", link: "/" },
+    { text: "Contact Us", link: "/" },
+    { text: "Speed Test", link: "/" },
+    { text: "Legal Notices", link: "/" },
+    { text: "Only on Netflix", link: "/" },
+    { text: "Do Not Sell or Share My Personal Information", link: "/" },
 ]
 
 function Footer() {
@@ -28,7 +28,7 @@ function Footer() {
                 <div className="mb-8">Questions? Call 1-844-505-2993</div>
                 <div className="grid justify-around grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-y-3 gap-x-2 text-sm">
                     {footerPages.map((page) => (
-                        <div>{page.text}</div>
+                        <div><a className="hover:underline" href={page.link}>{page.text}</a></div>
                     ))}
                 </div>
             </div>
