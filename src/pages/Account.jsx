62c96f035446e1e720a1f90c3e79f61fe4006d3e
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-04-16 15:19:07 -0400

    Retitle h2

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -23,7 +23,7 @@ const Account = () => {
             />
             <div className='bg-black/60 absolute top-0 left-0 w-full h-[550px]'></div>
             <div className='absolute top-[20%] p-4 md:p-8'>
-              <h1 className='text-3xl md:text-5xl font-bold'>My Shows</h1>
+              <h1 className='text-3xl md:text-5xl font-bold'>My Account</h1>
             </div>
           </div>
         ) : (
