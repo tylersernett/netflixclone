c6a92e03e6fc52e7f00b1a4944581d5396ecda9f
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-04-16 15:54:20 -0400

    Add Animation request

--- src/Requests.js
+++ src/Requests.js
@@ -5,7 +5,9 @@ const requests = {
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
     requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
-    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestAnimation: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&with_genres=16&language=en-US&page=1`,
+    requestHorror: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&with_genres=27&language=en-US&page=1`,
+    // requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
 }
 
 export default requests
