b8b8346fde11777ec0f4d332d16a81281949959c
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-05-31 00:37:19 -0400

    Add GuestButton component

---
+++ src/components/GuestButton.jsx
@@ -0,0 +1,24 @@
+import { UserAuth } from "../context/AuthContext";
+import { useNavigate } from 'react-router-dom'
+
+function GuestButton() {
+    const { guestLogIn } = UserAuth();
+    const navigate = useNavigate();
+
+    const handleGuestLogIn = async () => {
+        try {
+            await guestLogIn();
+            navigate('/');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <>
+            <button onClick={handleGuestLogIn} className='bg-purple-700 w-full py-3 my-6 rounded font-bold' >Guest Login</button>
+        </>
+    );
+}
+
+export default GuestButton;
