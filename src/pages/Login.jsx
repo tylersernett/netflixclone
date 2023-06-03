9ba8e71a3b68effb233be7887cb5c65c6ad4a77a
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-06-02 23:15:25 -0400

    Replace fixed form placement with absolute

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,8 +27,8 @@ const Login = () => {
         src='https://assets.nflxext.com/ffe/siteui/vlv3/79fe83d4-7ef6-4181-9439-46db72599559/be1d82ef-72c1-4a2a-8c16-5170169f9ef8/US-en-20221017-popsignuptwoweeks-perspective_alpha_website_medium.jpg'
         alt='/'
       />
-      <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
-      <div className='fixed w-full px-4 py-24 z-50'>
+      <div className='bg-black/60 absolute top-0 left-0 w-full h-screen'></div>
+      <div className='absolute w-full px-4 py-24 z-50'>
         <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
           <div className='max-w-[320px] mx-auto py-16'>
             <h1 className='text-3xl font-bold'>Sign In</h1>
@@ -37,12 +37,12 @@ const Login = () => {
               <input ref={emailRef} className='p-3 my-2 bg-gray-700 rounded' type='email' placeholder='Email' autoComplete='email' />
               <input ref={passwordRef} className='p-3 my-2 bg-gray-700 rounded' type='password' placeholder='Password' autoComplete='current-password' />
               <button className='bg-red-600 hover:bg-red-700 py-3 my-6 rounded font-bold'>Sign In</button>
-              <div className='flex justify-between items-center text-sm text-gray-500'>
+              <div className='flex justify-between items-center text-sm text-gray-400'>
                 <p><input className='mr-1' type='checkbox' />Remember Me</p>
                 <p>Need Help?</p>
               </div>
               <p className='py-8'><span className='text-gray-400'>New to Notflicks? &nbsp;</span>
-                <Link to='/signup'>
+                <Link to='/signup' className='hover:underline'>
                   Sign up now
                 </Link>
                 <span className='text-gray-400'>.</span>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -26,8 +26,8 @@ const Signup = () => {
           src='https://assets.nflxext.com/ffe/siteui/vlv3/79fe83d4-7ef6-4181-9439-46db72599559/be1d82ef-72c1-4a2a-8c16-5170169f9ef8/US-en-20221017-popsignuptwoweeks-perspective_alpha_website_medium.jpg'
           alt='/'
         />
-        <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
-        <div className='fixed w-full px-4 py-24 z-50'>
+        <div className='bg-black/60 absolute top-0 left-0 w-full h-screen'></div>
+        <div className='absolute w-full px-4 py-24 z-50'>
           <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
             <div className='max-w-[320px] mx-auto py-16'>
               <h1 className='text-3xl font-bold'>Sign Up</h1>
@@ -42,7 +42,7 @@ const Signup = () => {
                   <p>Need Help?</p>
                 </div>
                 <p className='py-8'><span className='text-gray-400'>Already subscribed to Notflicks? &nbsp;</span>
-                  <Link to='/login'>
+                  <Link to='/login' className='hover:underline'>
                     Sign In
                   </Link>
                 </p>
